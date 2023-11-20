f59cb7557cc37b569ef3d9b3cdbb10ab51548534
altermo <>
2023-11-20 12:43:37 +0100

    M lua/plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,7 @@
 local map=require'utils.keymap'
-local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
+local pckr_path=vim.fn.stdpath'data'..'/pckr/pckr.nvim'
 if not vim.loop.fs_stat(pckr_path) then
-  vim.fn.system({
-    'git',
-    'clone',
-    "--filter=blob:none",
-    'https://github.com/lewis6991/pckr.nvim',
-    pckr_path
-  })
+  vim.system{'git','clone','--filter=blob:none','https://github.com/lewis6991/pckr.nvim',pckr_path}:wait()
 end
 vim.opt.rtp:prepend(pckr_path)
 local function lcmd(cmds,header)
