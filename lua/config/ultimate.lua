7b76bee4d49d12c73d1c79a0f72f097240eb1b3d
altermo <107814000+altermo@users.noreply.github.com>
2024-07-07 13:56:06 +0200

    M  lua/config/ultimate.lua

--- lua/config/ultimate.lua
+++ lua/config/ultimate.lua
@@ -1,34 +1,30 @@
-vim.opt.runtimepath:prepend('/home/user/.config/nvim/.other/ua') --For testing
+vim.opt.runtimepath:prepend('/home/user/.config/nvim/.other/ua_') --For testing
 local upair=require'ultimate-autopair'
 _G.UA_DEV=true
-local configs={upair.extend_default{
+upair.setup({
   --space2={enable=true},
-  bs={space='balance',indent_ignore=true,single_delete=true,map={'<C-w>','<bs>'}},
-  cr={autoclose=true},
+  backspace={--[[space='balance',indent_ignore=true,single_delete=true]]},
+  newline={--[[autoclose=true]]},
   fastwarp={
-    multi=true,
-    {},
-    {
-      faster=true,
-      map='<C-A-e>',
-      cmap='<C-A-e>',
-      rmap='<C-A-S-e>',
-      rcmap='<C-A-S-e>',
-    },
+    --multi=true,
+    --{},
+    --{
+    --  faster=true,
+    --  map='<C-A-e>',
+    --  cmap='<C-A-e>',
+    --  rmap='<C-A-S-e>',
+    --  rcmap='<C-A-S-e>',
+    --},
   },
-  tabout={enable=true,hopout=true},
-  extensions={
-    --utf8=false,
-    cond={cond={function(fn) return not fn.in_macro() end}}
+  --tabout={enable=true,hopout=true},
+  filter={
+    function()
+      return vim.fn.reg_recording()=='' and vim.fn.reg_executing()==''
+    end,
   },
   {'*','*',multiline=false,ft={'markdown'}},
   {'**','**',multiline=false,ft={'markdown'}},
   {'$','$',multiline=false,ft={'markdown'}},
-  config_internal_pairs={{'[',']',cond=function (_,o) return o.line:sub(o.col-4,o.col)~='\\x1b' end}},
-},
-  {profile=require'ultimate-autopair.experimental.terminal'.init,{'{','}'}},
-  {profile=require'ultimate-autopair.experimental.matchpair'.init},
-}
-require'ultimate-autopair.core'.modes={'i','c','n','t'}
-upair.init(configs)
+  --change={{'[',']',filter={function (_,o) return o.line:sub(o.col-4,o.col)~='\\x1b' end}}},
+})
 
